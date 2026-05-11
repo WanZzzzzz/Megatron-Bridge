318cff588d4a335a7989016b8ee0b730f655e015
qiyuw <qiyuw@nvidia.com>
2026-05-11 12:39:22 -0700

    CG and eval
    Signed-off-by: qiyuw <qiyuw@nvidia.com>

--- src/megatron/bridge/training/train.py
+++ src/megatron/bridge/training/train.py
@@ -306,7 +306,7 @@ def train(
     # Wrap model with PagedStashRunner when moe_expert_rank_capacity_factor padding is enabled.
     # PagedStashRunner is responsible for detecting overflow and re-running iteration in eager-mode without padding.
     if HAS_PAGED_STASHING and config.model.moe_expert_rank_capacity_factor is not None:
-        copy_main_params = config.optimizer.reuse_grad_buf_for_mxfp8_param_ag and config.ddp.overlap_param_gather
+        copy_main_params = config.optimizer.reuse_grad_buf_for_mxfp8_param_ag
         forward_backward_func = PagedStashRunner(
             model_config, copy_main_params, model, optimizer, forward_backward_func
         )
@@ -594,7 +594,7 @@ def train(
             if energy_monitor is not None:
                 energy_monitor.pause()
             timers("interval-time").stop()
-            if config.optimizer.reuse_grad_buf_for_mxfp8_param_ag and config.ddp.overlap_param_gather:
+            if config.optimizer.reuse_grad_buf_for_mxfp8_param_ag:
                 # disable_forward_pre_hook(param_sync=True) below force-syncs params for eval.
                 # Copy the main params to param buffer before the forced AllGather.
                 for model_chunk in model:
